241ba5fbbe7ab2062c79e23041e6f4230d24c103
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2022-03-31 17:20:40 -0700

    replication working

--- data-cloning/RedisStorage.py
+++ data-cloning/RedisStorage.py
@@ -3,8 +3,6 @@ import redis as red
 import json, sys
 import itertools
 from importlib.metadata import version
-print("Redis Version")
-print(version('redis'))
 
 # Host redis process location, contains data to collect
 # Use the Raspberry Pi IP, should be static
@@ -13,11 +11,7 @@ redisHost = red.Redis(host='192.168.137.10', port=6379)
 # Client redis process location, contains replicaOf host
 redisClient = red.Redis(host='redis-db', port=6379)
 
-print("Is command callable?")
-command = getattr(redisClient, "replicaof", None)
-print(callable(command))
-
 # Duplicate the host onto the client
-# redisClient.xrange('192.168.137.10', '6379')
+redisClient.replicaof('192.168.137.10', '6379')
 
 print("container up", flush=True)
