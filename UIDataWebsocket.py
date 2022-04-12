ba3932f182fac50b05c7393bc3526a14ff4bed88
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2022-04-11 17:04:55 -0700

    Access operator PC redis REPLICAOF instead of Pi redis

--- UIDataWebsocket.py
+++ UIDataWebsocket.py
@@ -249,7 +249,7 @@ async def valve_producer(Loop_starter):
 
 # Create redis client for fetching data
 # Use the Raspberry Pi IP, should be static
-redis = red.Redis(host='192.168.137.10', port=6379)
+redis = red.Redis(host='localhost', port=6379)
 
 # While loop control variable
 Operation = True
